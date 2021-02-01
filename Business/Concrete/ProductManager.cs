d8face199a264152c4a654b1f27ed3043ff442f6
=Burak <=mbs199008@gmail.com>
2021-02-01 13:44:59 +0300

    GİT

---
+++ Business/Concrete/ProductManager.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Business.Abstract;
+using Entities.Concrete;
+using DataAccess.Abstract;
+
+namespace Business.Concrete
+{
+    public class ProductManager : IProductService
+    {
+         IProductDal _productDal;
+
+        public ProductManager(IProductDal productDal)
+        {
+            _productDal = productDal;
+        }
+
+        public List<Product> GetAll()
+        {
+           //iş kodları
+           return _productDal.GetAll();
+        }
+    }
+}
